794da13aa203f78d9a5b0aa61f964657ea082348
Luís Simas <luissimas@protonmail.com>
2024-04-18 16:17:05 -0300

    refactor: use new zmq lib

--- exercicio-1/go.mod
+++ exercicio-1/go.mod
@@ -2,4 +2,13 @@ module exercicio-1
 
 go 1.22.1
 
-require github.com/zeromq/goczmq v4.1.0+incompatible
+require (
+	github.com/go-zeromq/zmq4 v0.16.0
+	github.com/zeromq/goczmq v4.1.0+incompatible
+)
+
+require (
+	github.com/go-zeromq/goczmq/v4 v4.2.2 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+)
--- exercicio-1/server.go
+++ exercicio-1/server.go
@@ -1,24 +1,26 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"os"
 	"time"
 
-	"github.com/zeromq/goczmq"
+	zmq "github.com/go-zeromq/zmq4"
 )
 
 func main() {
 	// Creating the Router (server).
-	router, err := goczmq.NewRouter("tcp://*:6969")
+	router := zmq.NewRouter(context.Background())
+	err := router.Listen("tcp://*:6969")
 	if err != nil {
 		slog.Error("Could not create router", slog.Any("error", err))
 		os.Exit(1)
 	}
-	// Ensure that the router will be destroyed at the end of the
+	// Ensure that the router will be closed at the end of the
 	// program. This will be executed only at the end of the main
 	// function.
-	defer router.Destroy()
+	defer router.Close()
 
 	slog.Info("Created router")
 	durations := []time.Duration{}
@@ -29,37 +31,30 @@ func main() {
 		// Receive a message. In this case the first element of req is a
 		// routing id assigned by ZeroMQ and the second element is the
 		// message itself.
-		req, err := router.RecvMessage()
+		req, err := router.Recv()
 		start := time.Now()
 		if err != nil {
 			slog.Error("Could not receive message", slog.Any("error", err))
 			os.Exit(1)
 		}
 
-		slog.Info("Received message", slog.String("msg", string(req[1])))
+		msg := req.String()
+		slog.Info("Received message", slog.String("msg", msg))
 
 		// Send the first frame as req[0] to indicate the routing id
 		// of the message. The flag goczmq.FlagMore signals that the
 		// message contains more frames.
-		err = router.SendFrame(req[0], goczmq.FlagMore)
-		if err != nil {
-			slog.Error("Could not send first response frame", slog.Any("error", err))
-			os.Exit(1)
-		}
-
-		// Append a "*" to the message.
-		resp := append(req[1], byte('*'))
-
-		// Send the first response. The flag goczmq.FlagNone signals
-		// that this is the last frame of the message.
-		err = router.SendFrame(resp, goczmq.FlagNone)
+		respText := append(req.Frames[1], byte('*'))
+		resp := zmq.NewMsgFrom(req.Frames[0], respText)
+		err = router.Send(resp)
 		duration := time.Since(start)
 		if err != nil {
 			slog.Error("Could not send response", slog.Any("error", err))
 			os.Exit(1)
 		}
+
 		durations = append(durations, duration)
 		average = average + ((duration - average) / time.Duration(len(durations)))
-		slog.Info("Sent response", slog.String("resp", string(resp)), slog.Duration("processing_time", duration), slog.Duration("average", average))
+		slog.Info("Sent response", slog.String("resp", string(respText)), slog.Duration("processing_time", duration), slog.Duration("average", average))
 	}
 }
